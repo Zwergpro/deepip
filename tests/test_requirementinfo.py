305fe6a6b79b24257f05fa839ac64cdcd6acc0fb
Nesterenko <nestslock@gmail.com>
2021-11-13 17:26:05 +0300

    test(RequirementInfo): change any version

--- tests/test_requirementinfo.py
+++ tests/test_requirementinfo.py
@@ -8,27 +8,27 @@ class TestRequirementSpecifier:
         versions = ['<2.2.0', '>=1.0.1']
         requirement = RequirementInfo(requirement_factory(versions=versions))
 
-        assert requirement.specifier == ','.join(versions)
+        assert requirement.specifier == ','.join(versions), 'requirement should specify correct versions'
 
     def test_specifier_version_with_two_borders_and_reverse_order(self):
         versions = ['>=1.0.1', '<2.2.0']
         requirement = RequirementInfo(requirement_factory(versions=versions))
 
-        assert requirement.specifier == ','.join(reversed(versions))
+        assert requirement.specifier == ','.join(reversed(versions)), 'requirement should specify correct order'
 
     def test_specifier_version_with_three_borders(self):
         versions = ['<2.2.0', '>=0.5.0', '>=1.0.1']
         requirement = RequirementInfo(requirement_factory(versions=versions))
 
-        assert requirement.specifier == ','.join(versions)
+        assert requirement.specifier == ','.join(versions), 'requirement should specify correct versions'
 
     def test_specifier_version_with_one_border(self):
         versions = ['>=1.0.1']
         requirement = RequirementInfo(requirement_factory(versions=versions))
 
-        assert requirement.specifier == ','.join(versions)
+        assert requirement.specifier == ','.join(versions), 'requirement should specify correct version'
 
     def test_specifier_version_without_borders(self):
         requirement = RequirementInfo(requirement_factory(versions=[]))
 
-        assert requirement.specifier == ''
+        assert requirement.specifier == RequirementInfo.ANY_VERSION, 'requirement should specify Any version'
